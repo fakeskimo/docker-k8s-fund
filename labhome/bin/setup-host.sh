b30cfcd46da4caa20002a93a0e2aa17241704d08
fakeskimo <fakeskimo@gmail.com>
2018-08-20 08:43:43 +0900

    setup-host.sh 내용 추가

---
+++ labhome/bin/setup-host.sh
@@ -0,0 +1,22 @@
+
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
+
+
+# optional, but recommended
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA300B7755AFCFAE
+
+# add Typora's repository
+sudo add-apt-repository 'deb https://typora.io/linux ./'
+sudo apt-get update
+
+# install typora
+sudo apt-get install typora
+
+sudo apt-get install docker.io docker-compose vim 
+
+
+echo 'eval "$(minikube completion bash)"' >> ~/.bashrc
+echo 'eval "$(kubectl completion bash)"' >> ~/.bashrc
+
+cp -r ./user-home/* ~/
+
